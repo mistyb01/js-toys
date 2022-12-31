ef92c8591c0116fb54087de1e6631d8101b771e9
Misty Becker <mistyb01@uw.edu>
2022-12-31 07:20:06 -0800

    breathing bubble has outer circle

--- breathe/breathe.js
+++ breathe/breathe.js
@@ -5,49 +5,68 @@ const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
 
 
-function drawCircle(radius) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+function drawCircle(radius, fill, strokeColor) {
     context.beginPath();
     context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = '#ccffff';
+    context.fillStyle = fill;
     context.fill();
-    context.lineWidth = 2;
-    context.strokeStyle = '#33cccc';
+    context.lineWidth = 4;
+    context.strokeStyle = strokeColor;
     context.stroke();        
 }
 
+let isBreathing = false;
 const playBtn = document.querySelector('#play-btn');
-playBtn.addEventListener('click', inhale);
+playBtn.addEventListener('click', () => {
+    isBreathing = !isBreathing;
+    if (isBreathing) { inhale() }
+});
 
 let currRadius = 70;
 
 let circleText = document.getElementById('circle-text');
 
 function inhale() {
+    currRadius = 70;
     var i = 0;
     circleText.innerText = 'breathe in';
     var timer = setInterval(() => {
-        if (i == 4000) {
+        if (!isBreathing) {
+            clearInterval(timer);
+            context.clearRect(0, 0, canvas.width, canvas.height);
+        }
+        else if (i == 4000) {
             clearInterval(timer);
             exhale();
+        } else {
+            i += 50;                                
+            currRadius += 1;
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            drawCircle(150, 'transparent', '#ccffff');
+            drawCircle(currRadius, '#33cccc', 'transparent');                                                                                                                                                          
         }
-        i += 50;
-        currRadius += 1;
-        drawCircle(currRadius);
     }, 50);
 }
 
 function exhale() {
+    currRadius = 150;
     var i = 0;
     circleText.innerText = 'breathe out';
     var timer = setInterval(() => {
-        if (i == 4000) {
+        if (!isBreathing) {
+            clearInterval(timer);
+            context.clearRect(0, 0, canvas.width, canvas.height);
+        }
+        else if (i == 4000) {
             clearInterval(timer);
             inhale();
+        } else {
+            i += 50;
+            currRadius -= 1;
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            drawCircle(150, 'transparent', '#ccffff');
+            drawCircle(currRadius, '#33cccc', 'transparent');
         }
-        i += 50;
-        currRadius -= 1;
-        drawCircle(currRadius);
     }, 50);
 }
 
